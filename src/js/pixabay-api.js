854a5bb281264741e58eb4c0af464aaa62deaaa0
GalinaKucherenko <galina.mail.k.n@gmail.com>
2024-05-08 16:22:03 +0300

    3

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,9 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-
-const options = {
-	method: "GET"
-};
-
-fetch("<https://pixabay.com/api/>")
-  .then(response => {
-    if (!response.ok) {
-      iziToast.Error({message: 'Sorry, there are no images matching your search query. Please try again!'});
-    }
-    return response.json();
-  })
+export function searchPictures(searchTerm) {
+    return fetch(`https://pixabay.com/api/?key=43779125-040e3030fad6a4afa34a77542&q=${searchTerm}&image_type=photo&orientation=horizontal&safesearch=true`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        });
+}
